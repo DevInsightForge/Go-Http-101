dd4ab9060e3d5af647ff6b433ad1061838a02b74
Mofajjal Rasul <mofajjal.rasul@technonext.com>
2024-02-22 15:36:33 +0600

    middleware: update cors middleware for requests
    Signed-off-by: Mofajjal Rasul <mofajjal.rasul@technonext.com>

--- internal/application/middleware/cors.middleware.go
+++ internal/application/middleware/cors.middleware.go
@@ -10,34 +10,24 @@ var originAllowlist = []string{
 	"http://localhost:5173",
 }
 
-var methodAllowlist = []string{"GET", "POST", "DELETE", "OPTIONS"}
+var methodAllowlist = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if isPreflight(r) {
-			origin := r.Header.Get("Origin")
-			method := r.Header.Get("Access-Control-Request-Method")
-			if slices.Contains(originAllowlist, origin) && slices.Contains(methodAllowlist, method) {
-				w.Header().Set("Access-Control-Allow-Origin", origin)
-				w.Header().Set("Access-Control-Allow-Methods", strings.Join(methodAllowlist, ", "))
-				w.Header().Set("Access-Control-Allow-Credentials", "true")
-				w.Header().Add("Vary", "Origin")
-			}
-		} else {
-			// Not a preflight: regular request.
-			origin := r.Header.Get("Origin")
-			if slices.Contains(originAllowlist, origin) {
-				w.Header().Set("Access-Control-Allow-Origin", origin)
-				w.Header().Add("Vary", "Origin")
-				w.Header().Set("Access-Control-Allow-Credentials", "true")
+		origin := r.Header.Get("Origin")
+		if slices.Contains(originAllowlist, origin) {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Add("Vary", "Origin")
+
+			if r.Method == "OPTIONS" && r.Header.Get("Origin") != "" && r.Header.Get("Access-Control-Request-Method") != "" {
+				method := r.Header.Get("Access-Control-Request-Method")
+				if slices.Contains(methodAllowlist, method) {
+					w.Header().Set("Access-Control-Allow-Methods", strings.Join(methodAllowlist, ", "))
+					return
+				}
 			}
 		}
 		next.ServeHTTP(w, r)
 	})
-}
-
-func isPreflight(r *http.Request) bool {
-	return r.Method == "OPTIONS" &&
-		r.Header.Get("Origin") != "" &&
-		r.Header.Get("Access-Control-Request-Method") != ""
 }
